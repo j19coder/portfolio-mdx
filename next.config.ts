e5a6c4497defc9b8d76b34b9835c0a049d5f0a78
Joshua Nichols <j19.coder@gmail.com>
2026-01-30 22:19:15 -0500

    Add static export config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  output: "export",
 };
 
 const withMDX = createMDX({
